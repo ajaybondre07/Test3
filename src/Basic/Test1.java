7f7252c17fd8998e92829953ce70498b6e55a961
PC <PC@DESKTOP-SAK67PP>
2022-10-03 18:44:56 +0530

    First Commit

---
+++ src/Basic/Test1.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class Test1 
+{
+public static void main(String[] args) 
+{
+
+	System.out.println("Main Method Is Runnig");
+	
+	m1();
+}	
+
+
+public static void m1()
+{
+System.out.println("Hi");
+
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Ajay_GitHub {
+}
